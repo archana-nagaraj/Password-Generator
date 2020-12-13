8be87ef4b9bcfe2c14c9a9adf024f7a97979d9e1
Archana Nagaraja <johndoe@example.com>
2020-12-12 22:31:39 -0800

    bug fixing and included functions for the user input prompts

--- script.js
+++ script.js
@@ -1,53 +1,77 @@
 // Assignment code here
 
-//Collect user inputs and store them in seperate variables
-var totalCharacters = Number(prompt("How many characters would you like your password to be. Choose a number between 8 and 128."));
-while ((totalCharacters < 8 || totalCharacters > 128) || isNaN(totalCharacters))
-{
-  totalCharacters = window.prompt("Please choose between 8 to 128 chars");
-}
-var uppercaseCharacters = confirm("Click OK to confirm including uppercase characters");
-var lowercaseCharacters = confirm("Click OK to confirm including lowercase characters");
-var numericCharacters = confirm("Click OK to confirm including numeric characters");
-var specialCharacters = confirm("Click OK to confirm including special characters");
+// function to collect/validate the password total length 
+function totallen(){
+    var totalCharacters = Number(prompt("How many characters would you like your password to be. Choose a number between 8 and 128."));
+    while ((totalCharacters < 8 || totalCharacters > 128) || isNaN(totalCharacters)){
+      totalCharacters = window.prompt("Please choose between 8 to 128 chars");
+    }
+    return totalCharacters;
+  }
 
-while ((uppercaseCharacters === false )  && (lowercaseCharacters === false) && (numericCharacters ===false) && (specialCharacters=== false)) 
-{
-  alert("At least one character type should be selected to generate the password. Please go through the options again!")
-  uppercaseCharacters = confirm("Click OK to confirm including uppercase characters");
-  lowercaseCharacters = confirm("Click OK to confirm including lowercase characters");
-  numericCharacters = confirm("Click OK to confirm including numeric characters");
-  specialCharacters = confirm("Click OK to confirm including special characters");
+  //function to check to include uppercase characters in the password
+function includeUppercase(){
+  var uppercaseCharacters = confirm("Click OK to confirm including uppercase characters");
+  return uppercaseCharacters;
+}
+  //function to check to include lowercase characters in the password
+function includeLowercase(){
+  var lowercaseCharacters = confirm("Click OK to confirm including lowercaseCharacters characters");
+  return lowercaseCharacters;
+}
+ //function to check to include numeric characters in the password
+function includeNumeric(){
+  var numericCharacters = confirm("Click OK to confirm including numericCharacters characters");
+  return numericCharacters;
+}
+ //function to check to include special characters in the password
+function includeSpecialChars(){
+  var specialCharacters = confirm("Click OK to confirm including specialCharacters characters");
+  return specialCharacters;
 }
 
-// function to generate password based on user inputs
-function generatePassword() {
+//function to generate the password
+function generatePassword(){
+  var len = totallen();
+  var u = includeUppercase();
+  var l = includeLowercase();
+  var n = includeNumeric();
+  var s = includeSpecialChars();
   var charSetValues= [];
   var passwordGen;
-  if (lowercaseCharacters){
+//validating the charType inputs for having the user select atleast one type 
+  while ((u === false )  && (l === false) && (n ===false) && (s=== false)) 
+    {
+      alert("At least one character type should be selected to generate the password. Please go through the options again!")
+      u = confirm("Click OK to confirm including uppercase characters");
+      l = confirm("Click OK to confirm including lowercase characters");
+      n = confirm("Click OK to confirm including numeric characters");
+      s = confirm("Click OK to confirm including special characters");
+    }
+    //assigning the charsetvalues for each charType
+  if (l){
     charSetValues.push("abcdefghijklmnopqrstuvwxyz");
   }
-  if (uppercaseCharacters){
+  if (u){
     charSetValues.push("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
   }
-  if (numericCharacters){
+  if (n){
     charSetValues.push("0123456789");
   }
-  if(specialCharacters) {
+  if(s) {
     charSetValues.push(" !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"); //Resource: The OWASP® Foundation site
   }
   // concatenating all the elements in the array
   charSetValues = charSetValues.join('');
   var passwordgenTemp = [];
-  for (var i=0; i < totalCharacters; i++){
+  //generating the random output based on the user input values
+  for (var i=0; i < len; i++){
     passwordgenTemp.push(charSetValues[Math.floor(Math.random() * charSetValues.length)]);
     // concatenating all the elements in the array
     passwordGen = passwordgenTemp.join('');
   }
   return passwordGen;
 }
-
-//generatePassword();
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -56,10 +80,8 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword());
